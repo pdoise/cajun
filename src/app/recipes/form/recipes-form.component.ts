d7a06520d95c174ba3ffce72036b8d6d001141a8
Phillip Doise <p.doise@gmail.com>
2023-05-06 14:23:19 -0600

    code review

--- src/app/recipes/form/recipes-form.component.ts
+++ src/app/recipes/form/recipes-form.component.ts
@@ -4,13 +4,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 import { Recipe } from 'src/app/shared/shared.models';
-import { RecipeService } from "../../recipes/recipes.service";
 
 import { environment } from 'src/environments/environment';
 
 @Component({
-    templateUrl: './recipes-form.component.html',
-    styleUrls: ['./recipes-form.component.scss']
+  templateUrl: './recipes-form.component.html',
+  styleUrls: ['./recipes-form.component.scss']
 })
 
 export class RecipeFormComponent implements OnInit {
@@ -24,7 +23,6 @@ export class RecipeFormComponent implements OnInit {
     private formBuilder: FormBuilder, private http: HttpClient,
     private router: Router,
     private route: ActivatedRoute,
-    private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
@@ -49,43 +47,22 @@ export class RecipeFormComponent implements OnInit {
   }
 
   saveRecipe(): void {
-    console.log(this.image)
-    if (this.image) {
-      const formData = new FormData();
-      formData.append('recipe[name]', this.recipeForm.get('name')?.value);
-      formData.append('recipe[description]', this.recipeForm.get('description')?.value);
-      formData.append('recipe[ingredients]', this.recipeForm.get('ingredients')?.value);
-      formData.append('recipe[directions]', this.recipeForm.get('directions')?.value);
-      formData.append('recipe[user_id]', this.recipeForm.get('user_id')?.value);
-      formData.append('recipe[image]', this.image);
-      if (this.isEdit) {
-        this.http.put(this.recipesURl + `/${this.recipe.id}`, formData).subscribe(response => {
-          this.router.navigate([`recipe/${this.recipe.id}`]);
-        });
-      } else {
-        this.http.post(this.recipesURl, formData).subscribe(response => {
-          this.router.navigate([`landing`]);
-        });
-      }
+    const formData = new FormData();
+    formData.append('recipe[name]', this.recipeForm.get('name')?.value);
+    formData.append('recipe[description]', this.recipeForm.get('description')?.value);
+    formData.append('recipe[ingredients]', this.recipeForm.get('ingredients')?.value);
+    formData.append('recipe[directions]', this.recipeForm.get('directions')?.value);
+    formData.append('recipe[user_id]', this.recipeForm.get('user_id')?.value);
+    if (this.image) { formData.append('recipe[image]', this.image);}
+    if (this.isEdit) {
+      this.http.put(this.recipesURl + `/${this.recipe.id}`, formData).subscribe(response => {
+        this.router.navigate([`recipe/${this.recipe.id}`]);
+      })
     } else {
-      if (this.isEdit) {
-        this.updateRecipe();
-      } else {
-        this.addRecipe();
-      }
+      this.http.post(this.recipesURl, formData).subscribe(response => {
+        this.router.navigate([`landing`]);
+      })
     }
-  }
-
-  addRecipe(): void {
-    this.recipeService.addRecipe(this.recipeForm.getRawValue()).subscribe(()=>{
-      this.router.navigate([`landing`]);
-    });
-  }
-
-  updateRecipe(): void {
-    this.recipeService.updateRecipe(this.recipeForm.getRawValue()).subscribe(()=>{
-      this.router.navigate([`recipe/${this.recipe.id}`]);
-    });
   }
 
 }
